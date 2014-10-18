010f47f516cfb6522383917134bd56ea3c88fe17
iJaack <giacomobarbieri94@gmail.com>
2014-10-18 21:03:29 +0200

    new commit

---
+++ testC/testC/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  testC
+//
+//  Created by Giacomo Barbieri on 18/10/14.
+//  Copyright (c) 2014 Giacomo Barbieri. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	String nome;
+	printf("Enter your name:");
+	scanf("%s", &nome);
+	printf("Hello, World!\n");
+    return 0;
+}
