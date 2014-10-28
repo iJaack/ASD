d91dfb42cb74c2eb0e85e5fd3a3d1c2263148886
iJaack <giacomobarbieri94@gmail.com>
2014-10-28 12:09:29 +0100

    esercitazione new v1.8

--- testC/Es201014/voto.c
+++ testC/Es201014/voto.c
@@ -21,10 +21,31 @@ int numVoti(voti lista) { // controlla il numero di voti
 }
 int nonValidi(voti lista_v, candidati lista_c) { //controlla i voti non validi
 	int count = 0;
+	int uguali = 1;
+	while (lista_v->next != '\0') {
+		while(lista_c->next != '\0' || !uguali) {
+			if(!(confronta(lista_v->info.codice, lista_c->info.cod))) {
+				uguali = 0;
+				count++;
+			}
+			lista_c = lista_c->next;
+		}
+		lista_v = lista_v->next;
+	}
 	return count;
 }
 int votiCandidato(voti lista_v, candidati lista_c, string nome_c, string cognome_c){ // restituisce i voti del candidato in input
 	int count = 0;
+	while (lista_v->next != '\0') {
+		while(lista_c->next != '\0') {
+			if(confronta(lista_c->info.nome, nome_c) && confronta(lista_c->info.cognome, cognome_c)) {
+				count++;
+			}
+			lista_c = lista_c->next;
+		}
+		lista_v = lista_v->next;
+	}
+
 	return count;
 }
 void removeInvalid(voti* lista_v, candidati lista_c) { //rimuove i voti non validi facendo il check con i codici
