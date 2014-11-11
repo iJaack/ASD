c93f624ac187ca02ca9172cf2a18e809bbdf929d
Giacomo Barbieri <gia.barbieri1@stud.uniroma3.it>
2014-11-11 17:57:15 +0100

    Create valore_massimo.h

---
+++ testC/valore_massimo.h
@@ -0,0 +1,16 @@
+//
+//  valore_massimo.h
+//  testC
+//
+//  Created by Giacomo Barbieri on 11/11/14.
+//  Copyright (c) 2014 Giacomo Barbieri. All rights reserved.
+//
+
+#ifndef testC_header_h
+#define testC_header_h
+#include <stdio.h>
+#include <stdlib.h>
+
+int valore_massimo(int**, int, int);
+
+#endif
